438e613b4169072d1f6aa2cb4298ea267a03e1c0
FynnJason <410132905@qq.com>
2017-12-14 15:04:58 +0800

    增加公共常量，减少代码冗余

--- app/src/main/java/com/fynnjason/kotlingank/adapter/ContentAdapter.kt
+++ app/src/main/java/com/fynnjason/kotlingank/adapter/ContentAdapter.kt
@@ -7,6 +7,7 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.fynnjason.kotlingank.R
 import com.fynnjason.kotlingank.beans.GankBean
+import com.fynnjason.kotlingank.config.Values
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.material_design_iconic_typeface_library.MaterialDesignIconic
 
@@ -24,25 +25,25 @@ class ContentAdapter(layoutResId: Int, data: MutableList<GankBean>?) : BaseQuick
         //判断icon类型
         val icon = helper?.getView<ImageView>(R.id.iv_type)
         when (item?.type) {
-            "all" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.ALL -> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_view_comfy))
-            "Android" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.ANDROID -> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_android))
-            "iOS" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.IOS -> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_apple))
-            "前端" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.FRONT_END-> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_language_javascript))
-            "休息视频" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.REST_VIDEO -> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_collection_video))
-            "拓展资源" -> icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.EXPAND_RESOURSE -> icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_view_module))
-            "瞎推荐" ->icon?.setImageDrawable(IconicsDrawable(mContext)
+            Values.CASUAL_RECOMMEND ->icon?.setImageDrawable(IconicsDrawable(mContext)
                     .color(mContext.resources.getColor(R.color.colorAccent))
                     .icon(MaterialDesignIconic.Icon.gmi_view_day))
         }
---
+++ app/src/main/java/com/fynnjason/kotlingank/config/Values.kt
@@ -0,0 +1,17 @@
+package com.fynnjason.kotlingank.config
+
+/**
+ * author：FynnJason
+ * copyright：© 2017 Android.Own.
+ */
+class Values {
+    companion object {
+        val ALL = "all"
+        val ANDROID="Android"
+        val IOS="iOS"
+        val FRONT_END="前端"
+        val REST_VIDEO="休息视频"
+        val EXPAND_RESOURSE="拓展资源"
+        val CASUAL_RECOMMEND="瞎推荐"
+    }
+}
